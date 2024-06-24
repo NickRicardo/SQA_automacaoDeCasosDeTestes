859eed4de15ea9ce8a0a6521c7cc8901088a65b5
Nícolas Santos <nicollas.ricardo@hotmail.com>
2024-06-24 18:04:01 -0300

    O

---
+++ src/test/java/br/edu/faa/testefaa/page/BasePage.java
@@ -0,0 +1,18 @@
+package br.edu.faa.testefaa.page;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+public abstract class BasePage {
+
+    //Driver base para todas as paginas de testes;
+    protected WebDriver driver;
+
+
+    protected BasePage(WebDriver driver){
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+
+}
---
+++ src/test/java/br/edu/faa/testefaa/page/GooglePage.java
@@ -0,0 +1,10 @@
+package br.edu.faa.testefaa.page;
+
+import org.openqa.selenium.WebDriver;
+
+public class GooglePage extends BasePage{
+
+    protected GooglePage(WebDriver driver){
+        super(driver);
+    }
+}
